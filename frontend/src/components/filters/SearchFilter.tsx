7c852c212c57029bcdadeeaff1d21bb672e66cce
Nikos Spiropoulos <spiropoulos94@gmail.com>
2025-01-28 20:22:48 +0200

    clear searchbar

--- frontend/src/components/filters/SearchFilter.tsx
+++ frontend/src/components/filters/SearchFilter.tsx
@@ -1,5 +1,13 @@
-import { Box, InputAdornment, styled, TextField } from "@mui/material";
+import {
+  Box,
+  InputAdornment,
+  styled,
+  TextField,
+  IconButton,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Close"; // Import the Clear icon
+import { useState } from "react";
 
 interface SearchFilterProps {
   onSearch: (search: string) => void;
@@ -26,6 +34,13 @@ const Searchbar = styled(TextField)(() => ({
 }));
 
 const SearchFilter = ({ onSearch }: SearchFilterProps) => {
+  const [searchValue, setSearchValue] = useState<string>(""); // State for input value
+
+  const handleClear = () => {
+    setSearchValue(""); // Clear the input
+    onSearch(""); // Notify parent about the cleared input
+  };
+
   return (
     <Box
       pt={"24px"}
@@ -36,16 +51,26 @@ const SearchFilter = ({ onSearch }: SearchFilterProps) => {
         xs: "100%",
         md: "400px",
       }}
-      //   maxWidth={"400px"}
     >
       <Searchbar
         size="small"
-        placeholder="Search by title..."
-        onChange={(e) => onSearch(e.target.value)}
+        placeholder="Filter by title..."
+        value={searchValue} // Bind the input value to state
+        onChange={(e) => {
+          setSearchValue(e.target.value); // Update the state
+          onSearch(e.target.value); // Notify parent about the search input
+        }}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
               <SearchIcon />
+            </InputAdornment>
+          ),
+          endAdornment: searchValue && ( // Conditionally render the clear icon
+            <InputAdornment position="end">
+              <IconButton onClick={handleClear} size="small">
+                <ClearIcon />
+              </IconButton>
             </InputAdornment>
           ),
         }}
